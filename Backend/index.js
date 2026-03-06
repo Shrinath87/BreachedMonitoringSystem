c0e39fc6647709332e4c84e8dd37430a5aab35a6
Aman Sharma <narendraman1234@gmail.com>
2026-03-06 23:53:37 +0530

    Added auth middleware and updated backend setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { isAuth } from "./Middleware/isAuth";
+import cors from "cors"
 
 dotenv.config(); // Load .env variables
 
 const app = express();
-
+app.use(express.json());
+app.use(cookieParser()); 
+app.use(cors({
+   origin:"http://localhost:5173",
+   credentials: true,
+}))
 app.get("/", (req, res) => {
+    return res.status(200).json({ message: "It's working fine" });
+});
+
+app.get("/testmiddleware",isAuth, (req, res) => {
     return res.status(200).json({ message: "It's working fine" });
 });
 
