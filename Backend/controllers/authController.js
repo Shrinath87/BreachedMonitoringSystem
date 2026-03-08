534f1af1d195f6fe8a44a592f0f5b649b5327cd6
PRAJWALPGR <prajwalparimi@gmail.com>
2026-03-08 16:08:06 +0530

    feat: switch to MySQL with Prisma v5, add migration and seed

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,12 +1,8 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { Pool } = require("pg");
-const { PrismaPg } = require("@prisma/adapter-pg");
 const { PrismaClient } = require("@prisma/client");
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 exports.login = async (req, res) => {
   try {
--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,10 +1,6 @@
-const { Pool } = require("pg");
-const { PrismaPg } = require("@prisma/adapter-pg");
 const { PrismaClient } = require("@prisma/client");
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 const findUserByEmail = async (email) => {
   return await prisma.user.findUnique({ where: { email } });
